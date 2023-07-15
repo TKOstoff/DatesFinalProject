31298cdb3be78207675350546228a1f2103ac1a9
TKOstoff <135725151+TKOstoff@users.noreply.github.com>
2023-07-15 20:23:22 +0300

    Replaced condition with a loop.

--- src/DatesFinalProject.java
+++ src/DatesFinalProject.java
@@ -5,9 +5,11 @@ public class DatesFinalProject {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter which day of the week represents the 1st of January (type a number form 1 to 7): ");
         byte weekDayFirstJanuary = input.nextByte();
-        if (weekDayFirstJanuary < 1 || weekDayFirstJanuary > 7) {
-            System.out.println("Invalid day, enter another number: ");
-        }
+            while (weekDayFirstJanuary < 1 || weekDayFirstJanuary > 7) {
+                System.out.print("Invalid day, enter another number: ");
+                weekDayFirstJanuary = input.nextByte();
+            }
+        System.out.println(weekDayFirstJanuary);
     }
 
     }
